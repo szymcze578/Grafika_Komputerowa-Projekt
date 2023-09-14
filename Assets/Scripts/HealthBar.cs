caed40db85fb53c89bb8c14af70cf5754ad09ff0
wiktbob992 <wiktbob992@student.polsl.pl>
2023-09-14 13:52:57 +0200

    comments

--- Assets/Scripts/HealthBar.cs
+++ Assets/Scripts/HealthBar.cs
@@ -3,22 +3,30 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
+/// <summary>
+/// Reprezentuje pasek życia
+/// </summary>
 public class HealthBar : MonoBehaviour
 {
-
+    /// <summary>
+    /// Reprezentuje pasek na ekranie, gdzie wyświetlane jest życie
+    /// </summary>
     public Slider slider;
-    /*
-     * Ustawia pasek zycia
-     */
+
+    /// <summary>
+    /// Ustawia maksymalną liczbę punktów życia
+    /// </summary>
+    /// <param name="health"> Ilość punktów życia do ustawienia </param>
     public void SetMaxHealth(int health)
     {
         slider.maxValue = health;
         slider.value = health;
     }
 
-    /*
-     * Aktualizuje wartosc na pasku zycia
-     */
+    /// <summary>
+    /// Aktualizuje wartosc na pasku zycia
+    /// </summary>
+    /// <param name="health"> Ilość punktów życia do ustawienia </param>
     public void SetHealth(int health)
     {
         slider.value = health;
--- Assets/Scripts/LightFollow.cs
+++ Assets/Scripts/LightFollow.cs
@@ -2,16 +2,25 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// Reprezentuje ustawienia światła podążającego za graczem i definiującego jego pole widzenia
+/// </summary>
 public class LightFollow : MonoBehaviour
 {
 
+    /// <summary>
+    /// Reprezentuje obiekt gracza na scenie
+    /// </summary>
     public Transform Player;
 
+    /// <summary>
+    /// Definiuje wysokość, na jakiej umieszone nad graczem jest światło
+    /// </summary>
     float lightOffsetZ;
 
-    /*
-     * Ustawia pozycje swiatla, ktore znajduje sie za graczem
-     */
+    /// <summary>
+    /// Aktualizuje pozycję światła, aby podążało za graczem
+    /// </summary>
     void Update()
     {
         Vector3 m_lightPos = new Vector3(Player.position.x, gameObject.transform.position.y, Player.position.z + lightOffsetZ);
